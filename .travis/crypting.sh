036e5fc8e3243cddde1e94ff2a41f92f82d9a2aa
Justin Partain <jpartain89@gmail.com>
2017-03-04 02:38:49 -0600

    Updates to encrypted info for travis and other various tests for travis

--- .travis/crypting.sh
+++ .travis/crypting.sh
@@ -9,10 +9,12 @@ fi
 
 if [ "$1" = "decrypt" ]; then
   for SECRET in $vault_files; do
-    ANSIBLE_VAULT_PASSWORD_FILE=tests/.vault_pass.txt ansible-vault view $SECRET > ${SECRET::-4}
+    openssl aes-256-cbc -k "$vault_file_pass" -in tests/.vault_pass.enc -out tests/.vault_pass.txt -d
+    ANSIBLE_VAULT_PASSWORD_FILE=tests/.vault_pass.txt ansible-vault decrypt "$vault_files"
   done
 elif [ "$1" = "clean" ]; then
   for SECRET in $vault_files; do
-    rm ${SECRET::-4}
+    ansible-vault encrypt $SECRET --vault-password-file=tests/.vault_pass.txt
   done
+  rm -rf tests/.vault_pass.enc
 fi
